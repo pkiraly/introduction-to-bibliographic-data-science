e024eff0970c4b0db47ea7ffb314060f285d70cb
Péter Király <pkiraly@gwdg.de>
2026-01-21 11:59:07 +0100

    Working with SQL #11

--- scripts/bdsutils.py
+++ scripts/bdsutils.py
@@ -1,14 +1,21 @@
 import urllib.request
 import zipfile
-import os
 
 target_dir = 'raw-data'
 
 def download_and_unzip(url, target_file):
+    """
+    Download a zip file, and uncompress it
+    
+    Parameters
+    ----------
+    url : str
+        The URL of the zip file to download
+    sound : str
+        The local file name
+    """
 
     urllib.request.urlretrieve(url, target_file)
 
     with zipfile.ZipFile(target_file, 'r') as zip_ref:
         zip_ref.extractall(target_dir)
-
-# os.rename(target_dir + '/data', target_dir + '/lnb')
--- scripts/ch3a-download-stn.py
+++ scripts/ch3a-download-stn.py
@@ -1,8 +1,13 @@
 from bdsutils import target_dir, download_and_unzip
+import os
 
 url = 'https://fbtee.uws.edu.au/stn/database/download/STN_database.zip'
 target_file = target_dir + '/stn_database.zip'
 
 download_and_unzip(url, target_file)
 
-# os.rename(target_dir + '/data', target_dir + '/lnb')
+directory = target_dir + '/fbtee'
+if not os.path.exists(directory):
+    os.mkdir(directory)
+
+os.rename(target_dir + '/chop_leeds_ac_uk.sql', directory + '/chop_leeds_ac_uk.sql')
