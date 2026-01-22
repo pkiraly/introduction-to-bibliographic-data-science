7e1e40451e43b6ea13b88c38e6e5368d817ab9eb
Péter Király <pkiraly@gwdg.de>
2026-01-22 11:50:37 +0100

    API: OAI-PMH #6

--- scripts/ch3a-oai.py
+++ scripts/ch3a-oai.py
@@ -1,5 +1,6 @@
 import io
 import sys
+import os
 from lxml import etree
 from sickle import Sickle
 
@@ -7,6 +8,8 @@ if len(sys.argv) == 1:
     print('Please give a directory name')
     exit()
 dir = sys.argv[1]
+if not os.path.exists(dir):
+    os.mkdir(dir)
 print('saving to %s' % (dir))
 
 namespaces = {
@@ -14,38 +17,43 @@ namespaces = {
     'marc21': 'http://www.loc.gov/MARC21/slim'
 }
 
-def write_output(output, i, dir):
+header = '<?xml version="1.0" encoding="utf8"?>' + "\n" + '<collection>' + "\n"
+footer = '</collection>'
+
+def write_output(output, file_counter, dir):
     """Writes MARC records to file"""
-    file_name = "%s/%06d.xml" % (dir, i)
+    file_name = "%s/%06d.xml" % (dir, file_counter)
     print(file_name)
     with io.open(file_name, 'w', encoding='utf8') as f:
         f.write(header)
         f.write("\n".join(output) + "\n")
         f.write(footer)
 
-sickle = Sickle('https://data.digar.ee/repox/OAIHandler', max_retries=4)
-
-header = '<?xml version="1.0" encoding="utf8"?>' + "\n" + '<collection>' + "\n"
-footer = '</collection>'
-
-output = []
-i = 0
+xmlrecords = []
+file_counteri = 0
 record_count = 0
+
+sickle = Sickle('https://data.digar.ee/repox/OAIHandler', max_retries=4)
 it = sickle.ListRecords(metadataPrefix='marc21xml', set='erb')
 for record in it:
     tree = etree.ElementTree(record.xml)
     token = tree.xpath('/resumptionToken', namespaces=namespaces)
 
-    recs = tree.xpath('/oai:record[*]/oai:metadata/marc21:record', namespaces=namespaces)
-    for rec in recs:
-        core = etree.tostring(rec, encoding='utf8', method='xml').decode("utf-8")
-        output.append(core.replace("<?xml version='1.0' encoding='utf8'?>\n", ''))
+    records = tree.xpath('/oai:record[*]/oai:metadata/marc21:record', namespaces=namespaces)
+    for rec in records:
+        xmlrecord = etree\
+               .tostring(rec, encoding='utf8', method='xml')\
+               .decode("utf-8")\
+               .replace("<?xml version='1.0' encoding='utf8'?>\n", '')
+        xmlrecords.append(xmlrecord)
         record_count += 1
-    if len(output) >= 100000:
-        write_output(output, i, dir)
-        output = []
-        i = i + 1
+    if len(xmlrecords) >= 100000:
+        write_output(xmlrecords, file_counter, dir)
+        xmlrecords = []
+        file_counter += 1
 
-write_output(output, i, dir)
-print('saved %d records to %d files' % (record_count, i))
+if len(xmlrecords) > 0:
+    write_output(xmlrecords, file_counter, dir)
+
+print('saved %d records to %d files' % (record_count, file_counter))
 
